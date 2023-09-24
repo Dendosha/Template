d492443095af9d3068e9bd32c295c898a7cd01d5
Dendosha <105165935+Dendosha@users.noreply.github.com>
2023-09-24 19:34:01 +0500

    feat: complete element mover js module

--- src/js/libs/elementMover.js
+++ src/js/libs/elementMover.js
@@ -1,12 +1,20 @@
-const elementMoverInit = (query, child, oldParent, newParent) => {
-	const mediaQuery = matchMedia(query)
+const rootFontSizeCheckElement = document.createElement('div')
+rootFontSizeCheckElement.innerText = 'Text to check font size'
+rootFontSizeCheckElement.ariaHidden = true
+rootFontSizeCheckElement.classList.add('--font-size-check')
+
+document.body.prepend(rootFontSizeCheckElement)
+
+const elementMoverInit = (queryType, querySize, child, oldParent, newParent) => {
+	let mediaQuery = matchMedia(`(${queryType}: ${querySize / 16}em)`)
 
 	document.addEventListener('DOMContentLoaded', moveElement)
 	mediaQuery.addEventListener('change', moveElement)
+	new ResizeObserver(moveElement).observe(rootFontSizeCheckElement)
 
 	function moveElement() {
 		if (mediaQuery.matches) newParent.append(child)
-		else if (!oldParent.contains(child)) oldParent.append(child)
+		else oldParent.append(child)
 	}
 }
 
